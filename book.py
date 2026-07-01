5e62535a13324451a5217856a3f34da7f7a7ee5e
ashwanth0322 <ashwanth2203@gmail.com>
2026-07-01 10:14:32 +0530

    Completed Library Management System using Python OOP and File Handling

--- book.py
+++ book.py
@@ -2,26 +2,30 @@
 class Book:
 
     '''attributes of the book'''
-    def __init__(self,title,author,genre,year):
+    def __init__(self,title,author,genre,year,availability):
         self.title=title
         self.author=author
         self.genre=genre
         self.year=year
+        self.availability=availability
 
     '''methods of book'''
     def save_to_file(self):
         with open("books.txt","a") as file:
-            file.write(f"{self.title},{self.author},{self.genre},{self.year}\n")
+            file.write(f"{self.title},{self.author},{self.genre},{self.year},{self.availability}\n")
 
     def display_books(self):
-        with open("books.txt","r") as file:
+        
+        with open("books.txt", "r") as file:
             for line in file:
                 data=line.split(",")
                 print("\nTitle of the book :",data[0])
                 print("Name of the Author :",data[1])
                 print("Genre of the book :",data[2])
                 print("Year of Publication :",data[3])
-                print("-" * 40)
+                print("Availability :",data[4])
+                print("-"*40)
+                
 
     def search_book(self,search):
         with open("books.txt","r") as file:
@@ -33,6 +37,7 @@ class Book:
                     print("Name of the Author :",data[1])
                     print("Genre of the book :",data[2])
                     print("Year of Publication :",data[3])
+                    print("Availability :",data[4])
                     print("-"*40)
                     found=True
             if found == False:
@@ -45,13 +50,14 @@ class Book:
             new_line=[]
             for line in lines:
                 data=line.split(",")
-                if delete == data[0]:
+                if delete.lower().strip() == data[0].lower().strip():
                     found=True
                 else:
                     new_line.append(line)
                 
         if found == False:
             print("No Book Found")
+            return
         with open("books.txt","w") as file:
             for line in new_line:
                 file.write(line)
@@ -70,8 +76,10 @@ class Book:
                         new_author=input("Enter the name of the author :")
                         new_genre=input("Enter the genre of the book :")
                         new_year=input("Enter the year of publication :")
-                        new_lines.append(f"{new_title},{new_author},{new_genre},{new_year}\n")
+                        new_availablity=input("Enter the availablity :")
+                        new_lines.append(f"{new_title},{new_author},{new_genre},{new_year},{new_availablity}\n")
                         found=True
+                        print(f"Updated the details of book {update}\n")
                     
                 else:
                     new_lines.append(line)
@@ -81,12 +89,58 @@ class Book:
         with open("books.txt","w") as file:
             for lines in new_lines:
               file.write(lines)
+
+
+    def issue_book(self):
+        with open("books.txt","r") as file:
+            issue=input("Enter the title of the book whose availablity has to be changed :")
+            lines=file.readlines()
+            new_lines=[]
+            found=False
+            for line in lines:
+                data=line.split(",")
+                if issue == data[0]:
+                    if data[4].strip() == "issued":
+                        print("The book is already issued")
+                    elif data[4].strip() == "available":
+                        data[4]="issued"
+                    found=True
+                    new_lines.append(f"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]}\n")
+                    print(f"The book {issue} is successfully isssed")
+                else:
+                    new_lines.append(line)
+            if found == False:
+                print("No Book Found")
+        with open("books.txt","w") as file:
+            for line in new_lines:
+                file.write(line)
+
+
+    def return_books(self):
+        with open("books.txt","r") as file:
+            return_book=input("Enter the title of the book that you want to return :")
+            new_lines=[]
+            found=False
+            for line in file:
+                data=line.split(",")
+                data[4]=data[4].strip()
+                if data[0].lower() == return_book.lower():
+                    if data[4].lower() == "issued":
+                        data[4]="available"
+                        new_lines.append(f"{data[0]},{data[1]},{data[2]},{data[3]},{data[4]}\n")
+                        found=True
+                        
+                    elif data[4].lower() == "available":
+                        print("The Book is already returned")
+                        new_lines.append(line)
+                        found=True
+                    print(f"The book {return_book} is returned successfully\n")
+                else:
+                    new_lines.append(line)
+            if found == False:
+                print("No Book Found")
             
-
-                    
-
-                   
-               
-
-
+        with open("books.txt","w") as file:
+            for line in new_lines:
+                file.write(line)
 
--- main.py
+++ main.py
@@ -7,8 +7,9 @@ while True:
     print("3. Search Books")
     print("4. Delete Books")
     print("5. Update Book")
-    print("6. Exit")
-
+    print("6. Issue Book")
+    print("7. Return Book")
+    print("8. Exit")
     try:
 
         menu=int(input("Enter the menu :"))
@@ -17,33 +18,47 @@ while True:
             author=input("Enter the name of the author :")
             genre=input("Enter the genre of the book :")
             year=int(input("Enter the year of publication :"))
+            availability=input("Enter the availabilty of the book :")
 
-            b1=book.Book(title,author,genre,year)
+            b1=book.Book(title,author,genre,year,availability)
             b1.save_to_file()
             print("Books Added successfully")
 
         elif menu == 2:
-            b=book.Book("","","",0)
+            print("These are the available books\n")
+            b=book.Book("","","",0,"")
             b.display_books()
+            
 
         elif  menu == 3:
             search=input("Enter the name of the Book to be Searched :")
-            b=book.Book("","","",0)
+            print(f"\nFound the Book with Title {search}")
+            b=book.Book("","","",0,"")
             b.search_book(search)
 
         elif menu == 4:
             delete=input("Enter the Title of the book to be deleted :")
-            b=book.Book("","","",0)
+            b=book.Book("","","",0,"")
             b.delete_book(delete)
+            print(f"The book Titled {delete} is deleted successfully\n")
 
         elif menu == 5:
-            b=book.Book("","","",0)
+            b=book.Book("","","",0,"")
             b.update_book()
+            print(f"the details of book is updated\n")
 
         elif menu == 6:
+           b=book.Book("","","",0,"")
+           b.issue_book()
+           
+
+        elif menu == 7:
+             b=book.Book("","","",0,"")
+             b.return_books()
+
+        elif menu == 8:
             print("Thank you For visiting")
             break
-
            
 
         else:
